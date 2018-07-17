1a7c439fb0d0ed189de2087c59fd41fcfb548789
Yogeshwar Singh <ysthecool@gmail.com>
2018-07-17 13:22:45 +0530

    boilerplate v1 code check-in

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css'
+ReactDOM.render(<p>This is my boilerplate.</p>, document.getElementById('app')) 
